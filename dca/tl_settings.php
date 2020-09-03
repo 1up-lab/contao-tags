eb0b4bf01bbb1f8d658b434a0d89033ee4f7f3ab
David Greminger <david.greminger@1up.io>
2020-09-03 09:27:56 +0100

    Don't extend tl_setting

--- dca/tl_settings.php
+++ dca/tl_settings.php
@@ -8,7 +8,7 @@
  * @license LGPL-3.0+
  */
 
-$GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] .= ';{tags_legend},disabledTagObjects'; 
+$GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] .= ';{tags_legend},disabledTagObjects';
 
 /**
  * Add fields
@@ -17,25 +17,13 @@ $GLOBALS['TL_DCA']['tl_settings']['fields']['disabledTagObjects'] = array
 (
 	'label'                   => &$GLOBALS['TL_LANG']['tl_settings']['disabledTagObjects'],
 	'inputType'               => 'checkbox',
-	'options_callback'        => array('tl_settings_tags', 'getTagTables'),
+    'options_callback' => static function () {
+        $tables = array();
+        foreach ($GLOBALS['tags_extension']['sourcetable'] as $sourcetable)
+        {
+            $tables[$sourcetable] = $sourcetable;
+        }
+        return $tables;
+    },
 	'eval'                    => array('multiple'=>true)
 );
-
-class tl_settings_tags extends tl_settings
-{
-	/**
-	 * Return available tag tables
-	 *
-	 * @return array Array of tag tables
-	 */
-	public function getTagTables()
-	{
-		$tables = array();
-		foreach ($GLOBALS['tags_extension']['sourcetable'] as $sourcetable)
-		{
-			$tables[$sourcetable] = $sourcetable;
-		}
-		return $tables;
-	}
-}
-
